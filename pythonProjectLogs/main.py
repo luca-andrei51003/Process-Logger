0b284197131a87f4e4c9633976962d3ae9138295
luca-andrei51003 <luca.culcean@gmail.com>
2024-01-05 12:01:29 +0200

    updated services

--- pythonProjectLogs/main.py
+++ pythonProjectLogs/main.py
@@ -13,7 +13,7 @@ def random_log_time():
 def random_log_type():
     return random.choice(['INFO', 'DEBUG', 'ERROR'])
 
-def random_app_log():
+def random_app():
     return random.choice(['BackendApp', 'FrontendApp', 'API', 'SYSTEM'])
 
 def random_ms():
@@ -26,11 +26,14 @@ def random_success_log_message_ms(app, ms):
     return '{} has ran successfully in {}ms'.format(app, ms)
 
 def random_fail_log_message():
-    return '{} has failed after {}ms. Retrying... '.format(random_app_log(), random_ms())
+    return '{} has failed after {}ms. Retrying... '.format(random_app(), random_ms())
 
 def random_debug_log_message():
-    return '{} is still running, please wait... '.format(random_app_log())
-
+    return '{} is still running, please wait... '.format(random_app())
+maxRuntime = 0
+minRuntime = 31
+maxrTimestamp = None
+minrTimestamp = None
 f = open(r'output.txt', 'a')
 for x in range(10000):
     log_type = random_log_type()
@@ -42,8 +45,19 @@ for x in range(10000):
         f.write(to_write)
     else:
         log_time = random_log_time()
-        app = random_app_log()
+        app = random_app()
         to_write_running = "{} - [{}] - {}".format(log_time, log_type, random_success_log_message_running(app))
-        to_write_finish = "{} - [{}] - {}\n".format(log_time, log_type, random_success_log_message_ms(app, random_ms()))
+        runtime = random_ms()
+        
+        to_write_finish = "{} - [{}] - {}\n".format(log_time, log_type, random_success_log_message_ms(app, runtime))
         f.write(to_write_running)
         f.write(to_write_finish)
+        if (runtime > maxRuntime):
+            maxRuntime = runtime
+            maxrTimestamp = log_time
+        elif (runtime < minRuntime):
+            minRuntime = runtime
+            minrTimestamp = log_time
+        else:
+            pass
+print (minRuntime, maxRuntime, maxrTimestamp, minrTimestamp)
